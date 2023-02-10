635056301d073c80a557295ba8d3512c2be2ab87
CodeConscious <hello@codeconscio.us>
2023-02-10 14:22:24 +0900

    Create models for video downloads (#2)
    * Add basic logic
    
    * Add onig crate for regex
    
    * Add onig crate for regex
    
    * Fix onig grouping
    
    * Add final counts

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,50 @@
 use std::io;
 use std::io::Write;
 
+use crate::video::*;
+
+mod video;
+
 fn main() {
-    run();
+    let (successes, failures) = run();
+
+    println!(
+        "Done with {} success(es) and {} failure(s).",
+        successes, failures
+    )
 }
 
-fn run() {
+fn run() -> (u32, u32) {
     let quit_commands = ["q", "quit", "exit"];
+
+    let mut successes: u32 = 0;
+    let mut failures: u32 = 0;
 
     loop {
         let input = ask_input();
-        println!("Got \"{}\"!", input);
 
         if quit_commands.iter().any(|&cmd| cmd == input.to_lowercase()) {
-            println!("Quitting...");
-            return;
+            println!("\nQuitting...");
+            return (successes, failures);
+        }
+
+        let video = match VideoResource::build(input) {
+            Ok(v) => v,
+            Err(e) => {
+                println!("Failed to get a video resource: {}", e);
+                continue;
+            }
+        };
+
+        match download_video(video) {
+            Ok(_) => {
+                println!("Download complete!");
+                successes += 1;
+            }
+            Err(e) => {
+                println!("Download failed: {}", e.message);
+                failures += 1;
+            }
         }
     }
 }
@@ -23,7 +53,7 @@ fn ask_input() -> String {
     let mut guess = String::new();
 
     while guess.trim().is_empty() {
-        print!("■ Enter a YouTube URL or a command: "); // To support other platforms later.
+        print!("\n■ Enter a YouTube URL or a command: "); // To support other platforms later.
         io::stdout().flush().unwrap(); // Necessary to display the previous line immediately.
         std::io::stdin()
             .read_line(&mut guess)
@@ -31,4 +61,13 @@ fn ask_input() -> String {
     }
 
     guess.trim().to_string()
+}
+
+fn download_video(video: VideoResource) -> Result<(), VideoDownloadError> {
+    println!(
+        "Will download {} at {}",
+        video.resource_type,
+        video.full_resource_uri()
+    );
+    Ok(())
 }
---
+++ src/video.rs
@@ -0,0 +1,83 @@
+use std::fmt;
+
+pub enum VideoType {
+    YouTubeVideo,
+    YouTubePlaylist,
+    YouTubeChannel,
+}
+
+impl fmt::Display for VideoType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let text = match self {
+            VideoType::YouTubeVideo => "YouTube video",
+            VideoType::YouTubePlaylist => "YouTube playlist",
+            VideoType::YouTubeChannel => "YouTube channel",
+        };
+        write!(f, "{}", text)
+    }
+}
+
+pub struct VideoResource {
+    pub resource_type: VideoType,
+    pub resource_id: String,
+    pub url_base: String,
+}
+
+impl VideoResource {
+    pub fn build(url: String) -> Result<VideoResource, String> {
+        let result = VideoResource::determine_video_type(url);
+        match result {
+            Some(r) => {
+                return Ok(VideoResource {
+                    resource_type: r.0,
+                    resource_id: r.1,
+                    url_base: "test".to_string(),
+                })
+            }
+            None => Err("Invalid URL.".to_string()),
+        }
+    }
+
+    fn determine_video_type(url: String) -> Option<(VideoType, String)> {
+        let resource_regex_pairs = vec![
+            (
+                r"^[\w-]{11}$|(?<=v=|v\\=)[\w-]{11}|(?<=youtu\.be/).{11}",
+                VideoType::YouTubeVideo,
+            ),
+            (r"(?<=list=)[\w\-]+", VideoType::YouTubePlaylist),
+            (
+                r"(?:www\.)?youtube\.com/(?:c/|channel/|@|user/)[\w\-]+",
+                VideoType::YouTubeChannel,
+            ),
+        ];
+
+        for p in resource_regex_pairs {
+            let rgx = onig::Regex::new(p.0).unwrap();
+            match rgx.captures(&url) {
+                Some(capts) => {
+                    let position_pair = capts.pos(0);
+                    let matched_text = match position_pair {
+                        Some(v) => &url[v.0..v.1],
+                        None => panic!("Unexpected regex error"),
+                    };
+
+                    // println!("Matched text: {}", matched_text);
+                    return Some((p.1, matched_text.to_string()));
+                }
+                None => continue,
+            }
+        }
+
+        return None;
+    }
+
+    pub fn full_resource_uri(&self) -> String {
+        self.url_base.clone() + &self.resource_id
+    }
+}
+
+#[derive(Debug)]
+pub struct VideoDownloadError {
+    pub message: String,
+    pub inner: std::io::Error,
+}
